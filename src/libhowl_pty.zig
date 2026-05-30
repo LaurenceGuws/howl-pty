c928183c2fbe6f2c173375c20e8ebb64b100326d
Laurence <lggousie46@gmail.com>
2026-05-30 11:21:49 +0200

    pty: order abi exports

--- src/libhowl_pty.zig
+++ src/libhowl_pty.zig
@@ -5,12 +5,12 @@ comptime {
     @export(&ffi.sessionDeinit, .{ .name = "howl_pty_session_deinit" });
     @export(&ffi.sessionStart, .{ .name = "howl_pty_session_start" });
     @export(&ffi.sessionStop, .{ .name = "howl_pty_session_stop" });
-    @export(&ffi.sessionSnapshot, .{ .name = "howl_pty_session_snapshot" });
     @export(&ffi.sessionResize, .{ .name = "howl_pty_session_resize" });
     @export(&ffi.sessionPublishSignal, .{ .name = "howl_pty_session_publish_signal" });
     @export(&ffi.sessionPublishInput, .{ .name = "howl_pty_session_publish_input" });
     @export(&ffi.sessionPumpOutbound, .{ .name = "howl_pty_session_pump_outbound" });
     @export(&ffi.sessionKickWait, .{ .name = "howl_pty_session_kick_wait" });
+    @export(&ffi.sessionSnapshot, .{ .name = "howl_pty_session_snapshot" });
     @export(&ffi.sessionPendingBytes, .{ .name = "howl_pty_session_pending_bytes" });
     @export(&ffi.sessionBytesApplied, .{ .name = "howl_pty_session_bytes_applied" });
     @export(&ffi.sessionWaitReadable, .{ .name = "howl_pty_session_wait_readable" });
